96bfdb2d57e5425ca528fc0b20e57a0a98d711bc
kae-young <kaeyoung1@outlook.com>
2025-06-09 15:12:26 +1000

    add board header file to hold board variables

---
+++ src/drivers/board.h
@@ -0,0 +1,6 @@
+#pragma once
+
+#define LED_PIN 14
+#define ACCEL_SCL_PIN 2
+#define ACCEL_SDA_PIN 3
+#define SAD 0011001b
--- src/main.cpp
+++ src/main.cpp
@@ -6,10 +6,7 @@
 #include "WS2812.pio.h" // This header file gets produced during compilation from the WS2812.pio file
 #include "drivers/logging/logging.h"
 #include "drivers/led/led.h"
-
-#define LED_PIN 14
-
-
+#include "drivers/board.h"
 
 int main()
 {
